2bf9e67345ab4d4f44306e35dc8f401c19b77f5b
haakohu <Haak-hu@hotmail.com>
2016-03-21 16:20:35 +0100

    Reformed the select method for income and outcome

--- app/models/master_category.rb
+++ app/models/master_category.rb
@@ -7,4 +7,12 @@ class MasterCategory < ApplicationRecord
   def self.get_master_categories(user)
     MasterCategory.all.find_all{|t| t.user == nil or t.user == user}
   end
+
+  def self.get_list_with_master_and_subcat()
+    MasterCategory.all.find_all{|t|t.name != 'Income'}.map{|t| t.name}.map{|t| [t, Category.all.find_all{|f| f.master_category.name == t}.map{|f| [f.name,f.id]}]}
+  end
+
+  def self.get_list_with_income_subcat
+    MasterCategory.all.find_all{|t| t.name == "Income"}.map{|t| [t.name,Category.all.find_all{|f| f.master_category.name == t.name}.map{|f| [f.name,f.id]}]}
+  end
 end
